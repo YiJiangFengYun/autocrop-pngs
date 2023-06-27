4e0d1b982d0bfbcc446b4cf153331a852e1b07a8
luoyj <luoyj@suiyuan.info>
2023-06-27 10:49:51 +0800

    Add resize.js

---
+++ resize.js
@@ -0,0 +1,11 @@
+import jimp from "jimp";
+
+
+var pathTgt = process.argv[2];
+var width = Number(process.argv[3]);
+var height = Number(process.argv[4]);
+
+var lenna = await jimp.read(pathTgt);
+lenna.resize(width, height);
+await lenna.writeAsync(pathTgt);
+
