041de581c2d2d0daf5c645d11787772494e48b1b
luoyj <luoyj@suiyuan.info>
2024-08-28 10:43:19 +0800

    Add add_blank_around_dir

---
+++ add_blank_around_dir.js
@@ -0,0 +1,31 @@
+import jimp from "jimp";
+import fsExtra from "fs-extra";
+import path from "path";
+
+var dirSrc = process.argv[2];
+var dirOutput = process.argv[3];
+await fsExtra.ensureDir(dirOutput);
+await fsExtra.emptyDir(dirOutput);
+
+// 设置透明边框的宽度
+const borderWidth = 20;
+
+var files = await fsExtra.readdir(dirSrc);
+for (var file of files)
+{
+    if (path.extname(file).toLowerCase() === ".png")
+    {
+        var image = await jimp.read(path.join(dirSrc, file));
+        // 创建一个新的图像，大小为原图的宽度加上两倍的透明边框宽度，
+        // 高度同理
+        const newWidth = image.bitmap.width + borderWidth * 2;
+        const newHeight = image.bitmap.height + borderWidth * 2;
+
+        var blankImage = await jimp.create(newWidth, newHeight, 0x00000000);
+
+        blankImage.composite(image, borderWidth, borderWidth);
+        await blankImage.writeAsync(path.join(dirOutput, file));
+    }
+}
+
+
