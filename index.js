19ef8bcc9e713dff0d46bf83040361927f6c8018
luoyj <luoyj@suiyuan.info>
2025-01-06 15:00:48 +0800

    更新

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ import path from "path";
 import jimp from "jimp";
 
 
-var dirSrc = process.argv[2];
-var tgtSrc = process.argv[3];
-await fsExtra.ensureDir(tgtSrc);
-await fsExtra.emptyDir(tgtSrc);
+var srcDir = process.argv[2];
+var tgtDir = process.argv[3];
+await fsExtra.ensureDir(tgtDir);
+await fsExtra.emptyDir(tgtDir);
 
-var files = await fsExtra.readdir(dirSrc);
+var files = await fsExtra.readdir(srcDir);
 for (var file of files)
 {
     if (path.extname(file).toLowerCase() === ".png")
     {
-        var lenna = await jimp.read(path.join(dirSrc, file));
+        var lenna = await jimp.read(path.join(srcDir, file));
         lenna.autocrop();
-        await lenna.writeAsync(path.join(tgtSrc, file));
+        await lenna.writeAsync(path.join(tgtDir, file));
     }
 }
